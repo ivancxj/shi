9a17a24ff235670c54013eabef8609be45511dd9
ivan <peij@163.com>
2013-07-24 15:35:57 +0800

    1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Shiyimm::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( application.js device.js iscroll.js iscroll_box.js iscroll_page.js swipeview.js zepto.js application.css css.css public.css style.css )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
