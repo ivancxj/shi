574d91608f927cc047565c94a25d603ff7a1cebf
ivan <peij@163.com>
2013-08-02 12:47:14 +0800

    1

--- app/models/count_list.rb
+++ app/models/count_list.rb
@@ -1,3 +1,3 @@
-class Contact < ActiveRecord::Base
-  attr_accessible :owner_mobile, :contact_id, :mobile, :name
+class CountList < ActiveRecord::Base
+  #attr_accessible :owner_mobile, :contact_id, :mobile, :name
 end
